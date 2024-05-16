9b4c61e20a278077a5d1955dc35a92b7b3ad10f3
hasibuln27 <hasibulnative@gmail.com>
2024-05-16 16:58:01 +0600

    added NewsPortal Carousel,  - HomeScreen

---
+++ components/home/NewsPortalCarousel.jsx
@@ -0,0 +1,68 @@
+import { Dimensions, TouchableOpacity, View } from "react-native";
+import Carousel from "react-native-reanimated-carousel";
+import { useRef, useState } from "react";
+import BackgroundImage from "../shared/BackgroundImage";
+import { NewPortalImages } from "../../constants/Images";
+
+const PAGE_WIDTH = Dimensions.get("window").width;
+
+const CarousalItem = (index) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.75}
+      className="flex-1 rounded-2xl overflow-hidden"
+      style={{ height: 170 }}
+    >
+      <BackgroundImage source={NewPortalImages[index.index]} />
+    </TouchableOpacity>
+  );
+};
+
+const NewsPortalCarousel = () => {
+  const [isVertical, setIsVertical] = useState(false);
+  const baseOptions = isVertical
+    ? {
+        vertical: true,
+        width: PAGE_WIDTH * 0.86,
+        height: PAGE_WIDTH * 0.6,
+      }
+    : {
+        vertical: false,
+        width: PAGE_WIDTH,
+        height: 200,
+      };
+
+  const ref = useRef(null);
+
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        height: 200,
+      }}
+    >
+      <Carousel
+        ref={ref}
+        {...baseOptions}
+        style={{
+          width: PAGE_WIDTH,
+        }}
+        loop
+        pagingEnabled={true}
+        snapEnabled={true}
+        autoPlay={true}
+        scrollAnimationDuration={2500}
+        // onSnapToItem={(index) => console.log("current index:", index)}
+        mode="parallax"
+        modeConfig={{
+          parallaxScrollingScale: 0.9,
+          parallaxScrollingOffset: 50,
+        }}
+        data={[...new Array(3).keys()]}
+        renderItem={({ index }) => <CarousalItem index={index} />}
+      />
+    </View>
+  );
+};
+
+export default NewsPortalCarousel;
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -4,15 +4,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import Header from "../components/home/Header";
 import TopNews from "../components/home/TopNews";
+import NewsPortalCarousel from "../components/home/NewsPortalCarousel";
 
 const HomeScreen = () => {
   return (
     <View className="flex-1">
       <SafeAreaView className="bg-white/40 flex-1">
         <Header />
+        <NewsPortalCarousel />
         <TopNews />
       </SafeAreaView>
-
       <StatusBar style="dark" />
     </View>
   );
